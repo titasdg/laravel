300744338e609c237d7d3b7e379a2a1535c59453
Titas Dyglys <titasdg@gmail.com>
2019-04-02 11:19:45 +0300

    Template/routs

---
+++ resources/views/main.blade.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Colorlib Wordify &mdash; Minimal Blog Template</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300, 400,700|Inconsolata:400,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="{!!asset('css/bootstrap.css')!!}">
+    <link rel="stylesheet" href="{!!asset('css/animate.css')!!}">
+    <link rel="stylesheet" href="{!!asset('css/owl.carousel.min.css')!!}">
+
+    <link rel="stylesheet" href="{!!asset('fonts/ionicons/css/ionicons.min.css')!!}">
+    <link rel="stylesheet" href="{!!asset('fonts/fontawesome/css/font-awesome.min.css')!!}">
+    <link rel="stylesheet" href="{!!asset('fonts/flaticon/font/flaticon.css')!!}">
+
+    <!-- Theme Style -->
+    <link rel="stylesheet" href="{!!asset('css/style.css')!!}">
+  </head>
+  <body>
+    
+
+    <div class="wrap">
+
+      @include('partials/header')
+   
+      <!-- END section -->
+      <section class="site-section py-sm">
+    <div class="container">
+    @yield('content')
+    </div>
+     @include('partials/side')
+
+      </section>
+            <!-- END main-content -->
+
+       
+        @include('partials/footer')
+    <!-- loader -->
+    <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#f4b214"/></svg></div>
+
+    <script src="{!!asset('js/jquery-3.2.1.min.js')!!}"></script>
+    <script src="{!!asset('js/jquery-migrate-3.0.0.js')!!}"></script>
+    <script src="{!!asset('js/popper.min.js')!!}"></script>
+    <script src="{!!asset('js/bootstrap.min.js')!!}"></script>
+    <script src="{!!asset('js/owl.carousel.min.js')!!}"></script>
+    <script src="{!!asset('js/jquery.waypoints.min.js')!!}"></script>
+    <script src="{!!asset('js/jquery.stellar.min.js')!!}"></script>
+
+    
+    <script src="js/main.js"></script>
+  </body>
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,144 @@
+
+    <div class="wrap">
+<header role="banner">
+        <div class="top-bar">
+          <div class="container">
+            <div class="row">
+              <div class="col-9 social">
+                <a href="#"><span class="fa fa-twitter"></span></a>
+                <a href="#"><span class="fa fa-facebook"></span></a>
+                <a href="#"><span class="fa fa-instagram"></span></a>
+                <a href="#"><span class="fa fa-youtube-play"></span></a>
+              </div>
+              <div class="col-3 search-top">
+                <!-- <a href="#"><span class="fa fa-search"></span></a> -->
+                <form action="#" class="search-top-form">
+                  <span class="icon fa fa-search"></span>
+                  <input type="text" id="s" placeholder="Type keyword to search...">
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="container logo-wrap">
+          <div class="row pt-5">
+            <div class="col-12 text-center">
+              <a class="absolute-toggle d-block d-md-none" data-toggle="collapse" href="#navbarMenu" role="button" aria-expanded="false" aria-controls="navbarMenu"><span class="burger-lines"></span></a>
+              <h1 class="site-logo"><a href="index.html">Wordify</a></h1>
+            </div>
+          </div>
+        </div>
+        
+        <nav class="navbar navbar-expand-md  navbar-light bg-light">
+          <div class="container">
+            
+           
+            <div class="collapse navbar-collapse" id="navbarMenu">
+              <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                  <a class="nav-link active" href="index.html">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Business</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="category.html" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Travel</a>
+                  <div class="dropdown-menu" aria-labelledby="dropdown04">
+                    <a class="dropdown-item" href="category.html">Asia</a>
+                    <a class="dropdown-item" href="category.html">Europe</a>
+                    <a class="dropdown-item" href="category.html">Dubai</a>
+                    <a class="dropdown-item" href="category.html">Africa</a>
+                    <a class="dropdown-item" href="category.html">South America</a>
+                  </div>
+
+                </li>
+
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="category.html" id="dropdown05" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Categories</a>
+                  <div class="dropdown-menu" aria-labelledby="dropdown05">
+                    <a class="dropdown-item" href="category.html">Lifestyle</a>
+                    <a class="dropdown-item" href="category.html">Food</a>
+                    <a class="dropdown-item" href="category.html">Adventure</a>
+                    <a class="dropdown-item" href="category.html">Travel</a>
+                    <a class="dropdown-item" href="category.html">Business</a>
+                  </div>
+
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="about.html">About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="contact.html">Contact</a>
+                </li>
+              </ul>
+              
+            </div>
+          </div>
+        </nav>
+      </header>
+         <!-- END header -->
+
+         <section class="site-section pt-5 pb-5">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+
+              <div class="owl-carousel owl-theme home-slider">
+                <div>
+                  <a href="blog-single.html" class="a-block d-flex align-items-center height-lg" style="background-image: url('{!!asset('images/img_1.jpg')!!}'); ">
+                    <div class="text half-to-full">
+                      <span class="category mb-5">Food</span>
+                      <div class="post-meta">
+                        
+                        <span class="author mr-2"><img src="{!!asset('images/person_1.jpg')!!}" alt="Colorlib"> Colorlib</span>&bullet;
+                        <span class="mr-2">March 15, 2018 </span> &bullet;
+                        <span class="ml-2"><span class="fa fa-comments"></span> 3</span>
+                        
+                      </div>
+                      <h3>How to Find the Video Games of Your Youth</h3>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem nobis, ut dicta eaque ipsa laudantium!</p>
+                    </div>
+                  </a>
+                </div>
+                <div>
+                  <a href="blog-single.html" class="a-block d-flex align-items-center height-lg" style="background-image: url('{!!asset('images/img_2.jpg')!!}'); ">
+                    <div class="text half-to-full">
+                      <span class="category mb-5">Travel</span>
+                      <div class="post-meta">
+                        
+                        <span class="author mr-2"><img src="{!!asset('images/person_1.jpg')!!}" alt="Colorlib"> Colorlib</span>&bullet;
+                        <span class="mr-2">March 15, 2018 </span> &bullet;
+                        <span class="ml-2"><span class="fa fa-comments"></span> 3</span>
+                        
+                      </div>
+                      <h3>How to Find the Video Games of Your Youth</h3>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem nobis, ut dicta eaque ipsa laudantium!</p>
+                    </div>
+                  </a>
+                </div>
+                <div>
+                  <a href="blog-single.html" class="a-block d-flex align-items-center height-lg" style="background-image: url('{!!asset('images/img_3.jpg')!!}'); ">
+                    <div class="text half-to-full">
+                      <span class="category mb-5">Sports</span>
+                      <div class="post-meta">
+                        
+                        <span class="author mr-2"><img src="{!!asset('images/person_1.jpg')!!}" alt="Colorlib"> Colorlib</span>&bullet;
+                        <span class="mr-2">March 15, 2018 </span> &bullet;
+                        <span class="ml-2"><span class="fa fa-comments"></span> 3</span>
+                        
+                      </div>
+                      <h3>How to Find the Video Games of Your Youth</h3>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem nobis, ut dicta eaque ipsa laudantium!</p>
+                    </div>
+                  </a>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+          
+        </div>
+
+
+      </section>
